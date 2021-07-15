21f168f5b87aef107d2d8de59571bd08928833a6
gideon <gideonam@yahoo.com.br>
2021-07-15 00:21:08 -0300

    add scroll

--- main.js
+++ main.js
@@ -38,7 +38,24 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewheel: true, /* usa o scroll do mouse */
-    Keyboard: true /* abilita a seta do teclado */
-
-   
+    Keyboard: true /* abilita a seta do teclado */   
   });
+
+  /* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+  })
+  
+  scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+    { interval: 100 }
+  )
