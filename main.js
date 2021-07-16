aa7b75ec722dc4f6f6349492c9b26ea19a4d3fb4
gideon <gideonam@yahoo.com.br>
2021-07-16 00:38:07 -0300

    add quinta aula

--- main.js
+++ main.js
@@ -21,15 +21,18 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
+function changeHeaderWhenScroll() {
+
     if (window.scrolly >= navHeight) {
-    // scroll é maior que a altura do header    
-        header.classList.add('scroll')
-    }else {
-    // menor que a altura do header
-    header.classList.remove('scroll')    
-    }
-})
+        // scroll é maior que a altura do header    
+            header.classList.add('scroll')
+        }else {
+        // menor que a altura do header
+        header.classList.remove('scroll')    
+        }
+
+}
+
 
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -38,7 +41,14 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewheel: true, /* usa o scroll do mouse */
-    Keyboard: true /* abilita a seta do teclado */   
+    Keyboard: true, /* abilita a seta do teclado */  
+
+    breakpoints: { //Faz aparecer 2 cards no carrocel
+        767: {
+            slidesPerView:2,
+            setWarpperSize: true
+        }
+    }
   });
 
   /* ScrollReveal: Mostrar elementos quando der scroll na página */
@@ -59,3 +69,57 @@ const scrollReveal = ScrollReveal({
     `,
     { interval: 100 }
   )
+
+
+  /* Botão voltar para o topo */
+  const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+
+    if(window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+
+/* Menu ativo conforme a seção visível na páginaç */
+const sections = document.querySelectorAll('main section[id')
+function activateMenuAtCurrentSection() {
+
+    /* vai pegar o tamanho da tele e divipor o 8 vezes 4 */
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for ( const section of sections ) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+
+        }
+    }
+
+}
+
+
+
+
+/* When Scroll */
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll() 
+    backToTop()
+    activateMenuAtCurrentSection()
+})
+
